c0a37657c94e8a419fed6d3c8c5bb44bd0060b8a
Matías Nicolás Sepúlveda Ojeda <msepulveda@trisepsoft.onmicrosoft.com>
2025-11-01 16:22:00 -0300

    commit2

---
+++ src/main/java/com/empresa/proyecto_automatizacion/App.java
@@ -0,0 +1,10 @@
+package com.empresa.proyecto_automatizacion;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
